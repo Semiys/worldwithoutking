c40aff6a3f5eb7e40d03b920eb1854b7e888e814
Semiys <Semiysgladiys@gmail.com>
2024-09-29 05:33:39 +0400

    UI update

--- scripts/player_UI.gd
+++ scripts/player_UI.gd
@@ -1,48 +1,53 @@
 extends CanvasLayer
 
-var health_bar: ProgressBar
-var experience_bar: ProgressBar
-var level_label: Label
-var stats_label: Label
-var health_label: Label
-var experience_label: Label
+@onready var health_bar: ProgressBar = $HealthBar
+@onready var experience_bar: ProgressBar = $ExperienceBar
+@onready var level_label: Label = $LevelLabel
+@onready var stats_label: Label = $StatsLabel
+@onready var health_label: Label = $HealthLabel
+@onready var experience_label: Label = $ExperienceLabel
 
 func _ready():
-	create_ui_elements()
+	# 1. Настройка HealthBar:
+	#health_bar.set("theme_override_styles/fill", load("res://resources/health_bar_fg.tres"))
+	#health_bar.set("theme_override_styles/background", load("res://resources/health_bar_bg.tres"))
 
-func create_ui_elements():
-	health_bar = create_progress_bar(Vector2(10, 10), Vector2(200, 20), Color.RED)
-	experience_bar = create_progress_bar(Vector2(10, 40), Vector2(200, 10), Color.BLUE)
-	
-	level_label = create_label(Vector2(220, 10))
-	stats_label = create_label(Vector2(10, 60))
-	health_label = create_label(Vector2(10, 30))
-	experience_label = create_label(Vector2(10, 50))
+	# 2. Настройка ExperienceBar:
+	#experience_bar.set("theme_override_styles/fill", load("res://resources/exp_bar_fg.tres"))
+	#experience_bar.set("theme_override_styles/background", load("res://resources/exp_bar_bg.tres"))
 
-func create_progress_bar(position: Vector2, size: Vector2, color: Color) -> ProgressBar:
-	var bar = ProgressBar.new()
-	add_child(bar)
-	bar.set_position(position)
-	bar.set_size(size)
-	bar.set("theme_override_styles/fill", StyleBoxFlat.new())
-	bar.get("theme_override_styles/fill").set_bg_color(color)
-	return bar
+	# 3. Настройка меток:
+	#var main_font = load("res://resources/main_font.tres")
+	#level_label.set("theme_override_fonts/font", main_font)
+	#stats_label.set("theme_override_fonts/font", main_font)
+	#health_label.set("theme_override_fonts/font", main_font)
+	#experience_label.set("theme_override_fonts/font", main_font)
 
-func create_label(position: Vector2) -> Label:
-	var label = Label.new()
-	add_child(label)
-	label.set_position(position)
-	return label
+	# 4. Добавление иконок:
+	#var health_icon = TextureRect.new()
+	#health_icon.texture = load("res://assets/icons/health_icon.png")
+	#add_child(health_icon)
+	#health_icon.position = Vector2(10, 10)  # Настройте позицию
+
+	#var exp_icon = TextureRect.new()
+	#exp_icon.texture = load("res://assets/icons/exp_icon.png")
+	#add_child(exp_icon)
+	#exp_icon.position = Vector2(10, 40)  # Настройте позицию
+	pass
 
 func update_ui(player_data: Dictionary):
-	health_bar.value = player_data.health
+	animate_value(health_bar, player_data.health)
 	health_bar.max_value = player_data.max_health
 	
 	var experience_needed = player_data.level * 100
-	experience_bar.value = player_data.experience
+	animate_value(experience_bar, player_data.experience)
 	experience_bar.max_value = experience_needed
 	
 	level_label.text = "Уровень: " + str(player_data.level)
 	stats_label.text = "Атака: " + str(player_data.attack_power) + " | Защита: " + str(player_data.defense)
 	health_label.text = str(player_data.health) + " / " + str(player_data.max_health)
 	experience_label.text = str(player_data.experience) + " / " + str(experience_needed)
+
+func animate_value(progress_bar: ProgressBar, target_value: float):
+	var tween = create_tween()
+	tween.tween_property(progress_bar, "value", target_value, 0.5).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
