646b73b914cd82e680241ac8c2ee2e1ef40a5fb8
anteriorm <klmisha333@mail.ru>
2024-12-08 21:43:11 +0400

    Нпс дизайн
    Нпс дизайн

--- scripts/tutorial_master.gd
+++ scripts/tutorial_master.gd
@@ -1,31 +1,31 @@
 extends "res://scripts/npc.gd"
 
 func _ready():
-    npc_type = "tutorial_master"
-    super._ready()
+	npc_type = "tutorial_master"
+	super._ready()
 
 func setup_quests():
-    var tutorial_quests = [
-        Quest.new(
-            "Основы боя",
-            "Уничтожьте 5 тренировочных манекенов",
-            "kill_dummy",
-            5,
-            100
-        ),
-        Quest.new(
-            "Первая охота",
-            "Победите 3 слабых монстров",
-            "kill_weak",
-            3,
-            200
-        ),
-        Quest.new(
-            "Путь в большую деревню",
-            "Доберитесь до большой деревни",
-            "reach_village",
-            1,
-            300
-        )
-    ]
-    available_quests = tutorial_quests
+	var tutorial_quests = [
+		Quest.new(
+			"Основы боя",
+			"Уничтожьте 5 тренировочных манекенов",
+			"kill_dummy",
+			5,
+			100
+		),
+		Quest.new(
+			"Первая охота",
+			"Победите 3 слабых монстров",
+			"kill_weak",
+			3,
+			200
+		),
+		Quest.new(
+			"Путь в большую деревню",
+			"Доберитесь до большой деревни",
+			"reach_village",
+			1,
+			300
+		)
+	]
+	available_quests = tutorial_quests
